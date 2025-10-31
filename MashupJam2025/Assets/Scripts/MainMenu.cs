456972e377cd1a29dd33d8761b0ba1a0ef9e6f71
Poiroufe <matharany@gmail.com>
2025-10-31 18:17:26 +0100

    Reapply "menu buttons and title"
    This reverts commit c95a64f744e8a8f53dd30d8cc824228e704338aa.

--- MashupJam2025/Assets/Scripts/MainMenu.cs
+++ MashupJam2025/Assets/Scripts/MainMenu.cs
@@ -3,13 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- public void PlayGame()
+    [Header("Menu References")]
+    [SerializeField] private GameObject BackGround;
+    [SerializeField] private GameObject creditsbg;
+
+    public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
+
     public void QuitGame()
     {
         Application.Quit();
     }
 
+    public void ShowCredits()
+    {
+        creditsbg.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        creditsbg.SetActive(false);
+        BackGround.SetActive(true);
+    }
 }
